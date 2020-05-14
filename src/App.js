ed5b0ce700257228ab86a22e74692aa98527973b
pikktorr <viktor.rozgonyi@gmail.com>
2020-05-14 16:29:56 +0200

    create ACNH app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./img/New Horizons Logo.png";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Animal Crossing: New Horizons API</p>
+          <a
+            className="App-link"
+            href="http://acnhapi.com/doc"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            http://acnhapi.com/doc
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
