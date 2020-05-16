fe7873f65d913edc86e314940b2c4655f4641099
pikktorr <viktor.rozgonyi@gmail.com>
2020-05-16 22:21:23 +0200

    list of villagers

--- src/Components/Cards/CardList.js
+++ src/Components/Cards/CardList.js
@@ -2,19 +2,21 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ villagers }) => {
-  const villagersArray = Object.entries(villagers).map((villager) => {
-    const animalObject = villager[1];
-    return (
-      <Card
-        key={animalObject.id}
-        id={animalObject.id}
-        name={animalObject.name["name-en"]}
-        phrase={animalObject["catch-phrase"]}
-      />
-    );
-  });
-
-  return <div> {villagersArray}</div>;
+  return (
+    <div>
+      {Object.entries(villagers).map((villager) => {
+        const animalObject = villager[1];
+        return (
+          <Card
+            key={animalObject.id}
+            id={animalObject.id}
+            name={animalObject.name["name-en"]}
+            phrase={animalObject["catch-phrase"]}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default CardList;
