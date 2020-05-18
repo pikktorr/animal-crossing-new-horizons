fa885c23e9977d603a849da61b0aee59418bd816
pikktorr <viktor.rozgonyi@gmail.com>
2020-05-18 15:21:59 +0200

    Updates

---
+++ precache-manifest.6d46997a4d392b459d0e13c3d36aa751.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38f10df192df64fc2bfc0ea76374c481",
+    "url": "/villagers-of-animal-crossing-new-horizons/index.html"
+  },
+  {
+    "revision": "d30337318c5a6311f3a1",
+    "url": "/villagers-of-animal-crossing-new-horizons/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e601898cc1a4979750b6",
+    "url": "/villagers-of-animal-crossing-new-horizons/static/css/main.547bdb7d.chunk.css"
+  },
+  {
+    "revision": "d30337318c5a6311f3a1",
+    "url": "/villagers-of-animal-crossing-new-horizons/static/js/2.7f6aeca2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/villagers-of-animal-crossing-new-horizons/static/js/2.7f6aeca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e601898cc1a4979750b6",
+    "url": "/villagers-of-animal-crossing-new-horizons/static/js/main.7c77489a.chunk.js"
+  },
+  {
+    "revision": "a0c6558585ca61f9388b",
+    "url": "/villagers-of-animal-crossing-new-horizons/static/js/runtime-main.3ddfbffb.js"
+  },
+  {
+    "revision": "ee6b49807801a1ed884a8da9dedce088",
+    "url": "/villagers-of-animal-crossing-new-horizons/static/media/New Horizons Logo.ee6b4980.png"
+  },
+  {
+    "revision": "0ff5e57bec265d95ac2301dc50b12e16",
+    "url": "/villagers-of-animal-crossing-new-horizons/static/media/wallpaper_pattern.0ff5e57b.png"
+  }
+]);
